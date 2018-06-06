b7e83a172c01c3256fa8389be5b3487896f1044b
gmat191@gmail.com <gmat191@gmail.com>
2018-06-06 23:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function happyHolidaysTo(name) {
 }
 function happyHolidayTo(holiday){
   return(`Happy ${holiday} to ${name}!`)
-  if
-}
+  if (holiday){
+    
+  }
+  }
 function holdayCountdown( holiday, days) {
   return(`It's ${days} days until ${holiday}!`)
 }
